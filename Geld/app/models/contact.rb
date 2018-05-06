c4deb6158c23b410438d6d4ac52382f798a8fb02
bpeter340 <bartpeter@outlook.com>
2018-05-06 10:56:34 -0400

    Minor change to contact form

--- Geld/app/models/contact.rb
+++ Geld/app/models/contact.rb
@@ -2,6 +2,7 @@ class Contact < MailForm::Base
 attribute :name,      :validate => true
 attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
 attribute :message,    :validate => true
+attribute :subject,    :validate => false
 
 
 # Declare the e-mail headers. It accepts anything the mail method
